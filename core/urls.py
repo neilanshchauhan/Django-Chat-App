6b8503cb92e50a9da8fac0f0e2f81a63efd1b758
Neilansh Chauhan <neilanshchauhan4@gmail.com>
2023-10-08 17:37:54 +0530

    Create Login

--- core/urls.py
+++ core/urls.py
@@ -5,6 +5,7 @@ from .import views
 urlpatterns = [
     path('',views.frontpage, name='frontpage' ),
     path('signup/',views.signup, name='signup' ),
-    path('logout/',auth_views.LogoutView.as_view(), name='logout' ),
+    path('login/',auth_views.LoginView.as_view(template_name='core/login.html'), name='login'),
+    path('logout/',auth_views.LogoutView.as_view(), name='logout'),
 
 ]

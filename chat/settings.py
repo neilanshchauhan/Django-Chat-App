8c4967bb0d778348fe9407089bb6e29584f22597
Neilansh Chauhan <neilanshchauhan4@gmail.com>
2023-10-08 20:36:45 +0530

    Create consumersusing django channels

--- chat/settings.py
+++ chat/settings.py
@@ -76,6 +76,12 @@ TEMPLATES = [
 WSGI_APPLICATION = 'chat.wsgi.application'
 ASGI_APPLICATION = 'chat.asgi.application'
 
+CHANNEL_LAYERS = {
+    'default' : {
+        'BACKEND' : 'channels.layers.InMemoryChannelLayer'
+    }
+}
+
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases

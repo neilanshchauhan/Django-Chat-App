b8d6382a5014329edc1ee17ebc0e31e5f453cfb0
Neilansh Chauhan <neilanshchauhan4@gmail.com>
2023-10-08 18:09:59 +0530

    Create Room Detail View

---
+++ room/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from .models import Room
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+@login_required
+def rooms(request):
+    rooms = Room.objects.all()
+    context = {'rooms':rooms}
+    return render(request,'room/rooms.html', context)
+
+@login_required
+def room(request, slug):
+    room = Room.objects.get(slug=slug)
+    # messages = Message.objects.filter(room=room)[0:25]
+    context = {'room':room}
+    return render(request, 'room/room.html', context)
